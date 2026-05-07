6953d607a58ac5c92a123d563bbdf2e765b932e0
abdallah-blank <abdallah.brahim@dawsoncollege.qc.ca>
2026-05-07 12:53:49 -0400

    OLED Setup

--- pbl/pbl.ino
+++ pbl/pbl.ino
@@ -1,17 +1,32 @@
-#include <SSD1306.h> //Library for OLED
+//Author: Abdallah Brahim
+//Dawson College - EET
+//Date 5/7/2026
+
+//Libaries
 #include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h> //Library to enable graphics
+#include <Adafruit_SSD1306.h> //Library for OLED
 #include <MFRC522.h> //Library for RFID
 
+//Set screen size
+Adafruit_SSD1306 oledscr = Adafruit_SSD1306(128, 64, &Wire); 
+
+
+
+//Set Variables
 long buzzerPin = 4;
 long buttonPin = 5;
 #define RST_PIN 9
 #define SS_PIN 10
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); //create MFRC522 instance.
+//create MFRC522 instance.
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   SPI.begin(); //initialize SPI bus
+  oledDebut();
   mfrc522.PCD_Init(); //initialize MFRC522 card
   pinMode(buttonPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
@@ -19,5 +34,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  oledscr.setTextSize(3);
+  oledscr.setTextColor(WHITE);
+  oledscr.println("Gay");
+  oledscr.display();
+}
 
+
+void oledDebut(){
+  oledscr.begin(SSD1306_SWITCHCAPVCC, 0x3C); //Address 0x3C for 128x64
+  oledscr.display();
+  delay(1000);
+  oledscr.clearDisplay();
 }

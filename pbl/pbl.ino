de3274c2a5b01dca1aea483c9c3b3a6316630fcf
abdallah-blank <abdallah.brahim@dawsoncollege.qc.ca>
2026-05-08 01:05:01 -0400

    Door locking/unlocking

--- pbl/pbl.ino
+++ pbl/pbl.ino
@@ -17,27 +17,87 @@ Adafruit_SSD1306 oledscr = Adafruit_SSD1306(128, 64, &Wire);
 //Set Variables
 long buzzerPin = 4;
 long buttonPin = 5;
-#define RST_PIN 9
-#define SS_PIN 10
+#define RST_PIN 49
+#define SS_PIN 53
+
+int wrongAttempts = 0;
 
 //create MFRC522 instance.
-MFRC522 mfrc522(SS_PIN, RST_PIN); 
+MFRC522 rfid(SS_PIN, RST_PIN); 
+MFRC522::MIFARE_Key key;  
+
 
 void setup() {
   Serial.begin(9600);
   SPI.begin(); //initialize SPI bus
   oledDebut();
-  mfrc522.PCD_Init(); //initialize MFRC522 card
+  rfid.PCD_Init(); //initialize MFRC522 card
   pinMode(buttonPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  oledscr.setTextSize(3);
-  oledscr.setTextColor(WHITE);
-  oledscr.println("Gay");
-  oledscr.display();
+  // OLED PART
+
+  
+  
+  //RFID PART
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! rfid.PICC_IsNewCardPresent())
+  return;  
+  // Verify if the NUID has been read
+  if ( ! rfid.PICC_ReadCardSerial())
+  return; 
+
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  rfid.PICC_HaltA(); // Halt PICC
+  //End of RFID CARD READING
+
+  //Display door unlocking/locking.
+  String tag = getHexString(rfid.uid.uidByte, rfid.uid.size);
+  bool isUnlocked = keyChecker(tag);
+
+  if(isUnlocked){
+    oledDisplay(3, WHITE, "Door");
+    oledscr.setCursor(0, 25);
+    oledDisplay(3,WHITE,"Opened");
+    oledscr.display();
+    delay(300);
+    for (int i = 5; i > 0; i--) {
+      Serial.println("Door closing in: ");
+      Serial.println(i);
+      delay(1000);
+    }
+    oledscr.clearDisplay();
+    isUnlocked = false;
+  }
+  if(!isUnlocked){
+    oledscr.setCursor(0, 0);
+    oledDisplay(3, WHITE ,"Door");
+    oledscr.setCursor(0, 25);
+    oledDisplay(3,WHITE,"Locked");
+    oledscr.display();
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+void oledDisplay(int size,char color,char text[]){
+  oledscr.setTextSize(size);
+  oledscr.setTextColor(color);
+  oledscr.println(text);
+
 }
 
 
@@ -47,3 +107,44 @@ void oledDebut(){
   delay(1000);
   oledscr.clearDisplay();
 }
+
+
+//Get the TAG in a Hex String format (BY CHATGPT)
+String getHexString(byte *buffer, byte bufferSize) {
+  String hexString = "";
+
+  for (byte i = 0; i < bufferSize; i++) {
+    if (buffer[i] < 0x10) {
+      hexString += "0"; // leading zero
+    }
+    hexString += String(buffer[i], HEX);
+  }
+
+  hexString.toUpperCase(); // optional
+  return hexString;
+
+}
+
+//Check if the correct key is used
+bool keyChecker(String tagged){
+  if(tagged == "6C7D3A5C"){
+    Serial.println("WOW");
+    return true;
+  }
+  else{
+    Serial.println("Im sad");
+    return false;
+  }
+}
+
+
+
+/*//////////////////////I WAS HERE*/
+void attempts(){
+  if(!keyChecker){
+    wrongAttempts++;
+
+  }
+}
+
+

a9870e3890b7311a7c4e2b07a046a7c6581c03fe
abdallah-blank <abdallah.brahim@dawsoncollege.qc.ca>
2026-05-13 17:14:12 -0400

    Adding comments + fixing indentation

--- pbl/pbl.ino
+++ pbl/pbl.ino
@@ -18,8 +18,6 @@ Adafruit_SSD1306 oledscr = Adafruit_SSD1306(128, 64, &Wire);
 long buzzerPin = 4;
 #define RST_PIN 49
 #define SS_PIN 53
-
-
 int wrongAttempts = 3;
 
 
@@ -40,8 +38,7 @@ void setup() {
 }
 
 void loop() {
-
-  //RFID PART
+  //RFID startup part (From example code)
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if ( ! rfid.PICC_IsNewCardPresent())
   return;  
@@ -52,21 +49,13 @@ void loop() {
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
   rfid.PICC_HaltA(); // Halt PICC
   
-  //End of RFID CARD READING
+  
+  
+
+
+  //Door unlocking/locking. Logic
   bool isUnlocked = keyChecker(tag);
-
-
-
-
-
-  //Display door unlocking/locking.
-
-
-
-
-
-
-
+  //When RFID reading matches, unlocks door for 5 seconds
   if(isUnlocked){
     oledscr.setCursor(0,0);
     oledDisplay(3, WHITE, "Door");
@@ -74,37 +63,40 @@ void loop() {
     oledDisplay(3,WHITE,"Open");
     oledscr.display();
     delay(300);
-    for (int i = 5; i > 0; i--) {
+    for (int i = 5; i > 0; i--) { //Loop for timer (how long door stays unlocked)
       Serial.println("Door closing in: ");
       Serial.println(i);
       delay(1000);
     }
+    //Sets everything to default (Locked, 3 attempts, no buzzer)
     oledscr.setCursor(0, 0);
     oledscr.clearDisplay();
     isUnlocked = false;
     wrongAttempts=3;
-    digitalWrite(buzzerPin, LOW);
-      
+    digitalWrite(buzzerPin, LOW);  
   }
-    if(!keyChecker(tag)){
+    //When Card read is wrong, buzzes a little, after 3 wrong readings starts buzzing until correct key is used + displays intruder alert)
+  if(!keyChecker(tag)){
     wrongAttempts--;
     Serial.print("You have ");
     Serial.print(wrongAttempts);
     Serial.println(" attempts left");
     oledscr.clearDisplay();
-    if(wrongAttempts == 0){
 
-      Serial.println("No attempts Left");
-      delay(200);
-      oledDisplay(2, WHITE, "INTRUDER");
-      oledscr.setCursor(0, 25);
-      oledDisplay(2,WHITE,"ALERT!!!");
-      oledscr.display();
-      delay(200);
-      digitalWrite(buzzerPin, 1);
+    digitalWrite(buzzerPin, 1);
+    delay(150);
+    digitalWrite(buzzerPin,0);
 
-
-    }
+   if(wrongAttempts == 0){
+    Serial.println("No attempts Left");
+    delay(200);
+    oledDisplay(2, WHITE, "INTRUDER");
+    oledscr.setCursor(0, 25);
+    oledDisplay(2,WHITE,"ALERT!!!");
+    oledscr.display();
+    delay(200);
+    digitalWrite(buzzerPin, 1);
+   }
   }
   else{
     lockedDoor();
@@ -115,7 +107,7 @@ void loop() {
 
 
 
-
+//Displays "DOOR LOCKED" on OLED display
 void lockedDoor(){
     oledscr.setCursor(0,0);
     oledDisplay(3, WHITE ,"Door");
@@ -127,7 +119,7 @@ void lockedDoor(){
 }
 
 
-
+//Prints text to the OLED display
 void oledDisplay(int size,char color,char text[]){
   oledscr.setTextSize(size);
   oledscr.setTextColor(color);
@@ -135,7 +127,7 @@ void oledDisplay(int size,char color,char text[]){
 
 }
 
-
+//Starts the OLED display
 void oledDebut(){
   oledscr.begin(SSD1306_SWITCHCAPVCC, 0x3C); //Address 0x3C for 128x64
   oledscr.display();
@@ -144,7 +136,7 @@ void oledDebut(){
 }
 
 
-//Get the TAG in a Hex String format (BY CHATGPT)
+//Get the RFID card tag in a Hex String format (BY CHATGPT)
 String getHexString(byte *buffer, byte bufferSize) {
   String hexString = "";
 
